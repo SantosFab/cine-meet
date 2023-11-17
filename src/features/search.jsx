e66de74f9a5015815c7631aaa010c0020bc003b1
SantosFab <fabriciosantos.dev@gmail.com>
2023-11-17 10:31:50 -0300

    corrigo search reducer

--- src/features/search.jsx
+++ src/features/search.jsx
@@ -3,23 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 export const inputValue = createSlice({
   name: "search",
   initialState: {
-    value: '',
+    value: "",
   },
   reducers: {
-    increment: (state, number) => {
-        console.log()
-      state.value += 1 + number.payload;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
     setInputValue: (state, action) => {
-      console.log(action.payload)
       state.value += action.payload;
     },
   },
 });
 
-export const { increment, decrement, setInputValue } = inputValue.actions;
+export const { setInputValue } = inputValue.actions;
 
 export default inputValue.reducer;
