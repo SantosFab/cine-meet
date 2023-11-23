28b097b0d38013c808d750074832efb973d5d2fc
SantosFab <fabriciosantos.dev@gmail.com>
2023-11-23 07:27:18 -0300

    Adiciono Unidecode e atualizo lógica de MovieURL

--- src/utils/moviesUrl.jsx
+++ src/utils/moviesUrl.jsx
@@ -1,2 +1,9 @@
-const movieUrl = (str) => `/Detail/${str?.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/^-+|-+$/g, '')}`
-export default movieUrl
+import unidecode from 'unidecode';
+
+const movieUrl = (str) => {
+  const normalizedStr = unidecode(str || '').toLowerCase();
+  console.log(normalizedStr)
+  return `/Detail/${normalizedStr.replace(/[^a-z0-9]+/g, '-').replace(/^-+|-+$/g, '')}`;
+};
+
+export default movieUrl;
