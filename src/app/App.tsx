686df27e0cb5c47c0d4d2882c7c294eace397082
SantosFab <fabriciosantos.dev@gmail.com>
2024-01-26 16:44:30 -0300

    refatoro codigo

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/header/Header";
+import Routes from "../routes/route/genre/Genre";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes />
+    </>
+  );
+}
+
+export default App;
