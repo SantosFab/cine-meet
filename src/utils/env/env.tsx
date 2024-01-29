0106a7d0aac912accc2660984b677628c63de059
SantosFab <fabriciosantos.dev@gmail.com>
2024-01-29 16:23:35 -0300

    add nova url

--- src/utils/env/env.tsx
+++ src/utils/env/env.tsx
@@ -1,15 +1,18 @@
+const urldefault: string = "default_url";
 export const apiKey = process.env.REACT_APP_API_KEY;
 
 export const apiToken = process.env.REACT_APP_API_TOKEN;
 
 export const urlMovies: string =
-  process.env.REACT_APP_API_URL_FETCH_MOVIES ?? "default_url";
+  process.env.REACT_APP_API_URL_FETCH_MOVIES ?? urldefault;
 
 export const urlSeries: string =
-  process.env.REACT_APP_API_URL_FETCH_SERIES ?? "default_url";
+  process.env.REACT_APP_API_URL_FETCH_SERIES ?? urldefault;
 
 export const urlSearch: string =
-  process.env.REACT_APP_API_URL_FETCH_SEARCH ?? "default_url";
+  process.env.REACT_APP_API_URL_FETCH_SEARCH ?? urldefault;
 
-export const urlBaseImg =
-  process.env.REACT_APP_API_URL_IMG_500W ?? "default_url";
+export const urlBaseImg = process.env.REACT_APP_API_URL_IMG_500W ?? urldefault;
+
+export const urlDetail =
+  process.env.REACT_APP_API_URL_FETCH_DETAIL ?? urldefault;
