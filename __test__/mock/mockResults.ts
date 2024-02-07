ea81d42160e629dbd7ce67fcf4beee0cf9078019
SantosFab <fabriciosantos.dev@gmail.com>
2024-02-07 09:33:32 -0300

    add mock de ResultsItem

---
+++ __test__/mock/mockResults.ts
@@ -0,0 +1,33 @@
+import { ResultItem } from "../../src/reducer/fetch/commonTypes/interface";
+
+export const mockResult: ResultItem[] = [
+  {
+    id: 866398,
+    title: "Beekeeper - Rede de Vingança",
+    overview:
+      "Clay está em busca de vingança e não vai parar até acabar com o sistema criminoso responsável pela morte de sua amiga. O plano brutal de Clay, um ex-agente de uma organização secreta poderosa conhecida como “Beekeepers”, ganha dimensões nacionais envolvendo governos e as instituições mais influentes do mundo. Determinado a fazer justiça com suas próprias mãos, ele está disposto a tudo e não vai poupar ninguém para concluir seu plano.",
+    poster_path: "/cCGFvoxzaBbYM3I68s6F939fybN.jpg",
+  },
+  {
+    id: 787699,
+    title: "Wonka",
+    overview:
+      "Um jovem Willy Wonka embarca em uma missão para espalhar alegria por meio de seu chocolate, que rapidamente se torna um fenômeno.",
+    poster_path: "/5IiODXjfLhVk5OhBlAqueCB4GQv.jpg",
+  },
+  {
+    id: 933131,
+    title: "Em Ruínas",
+    overview:
+      "Após um terremoto transformar Seul em uma terra sem lei, um caçador decide resgatar uma adolescente sequestrada por um médico insano.",
+    poster_path: "/vT3xahepzP796ei0NiZIZ6t8vsl.jpg",
+  },
+  {
+    id: 572802,
+    title: "Aquaman 2: O Reino Perdido",
+  },
+  {
+    id: 940551,
+    title: "Patos!",
+  },
+];
