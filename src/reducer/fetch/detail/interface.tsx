7d20927b49b90c175b7cbd57519634e103209890
SantosFab <fabriciosantos.dev@gmail.com>
2024-02-08 08:21:18 -0300

    add nova interface ao DetailMovie

--- src/reducer/fetch/detail/interface.tsx
+++ src/reducer/fetch/detail/interface.tsx
@@ -5,13 +5,18 @@ export interface Descriptor {
   name: string;
 }
 
+export interface Countries {
+  iso_3166_1: string;
+  name: string;
+}
+
 export interface DetailData extends ResultItem {
   tagline?: string;
   original_title: string;
   original_language: string;
   genres?: Descriptor[];
   production_companies?: Descriptor[];
-  production_countries?: Descriptor[];
+  production_countries?: Countries[];
   release_date?: string;
   popularity: string;
 }
