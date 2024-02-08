bd6d6ea379897f0193100e26bcca77d1acffec08
SantosFab <fabriciosantos.dev@gmail.com>
2024-02-08 08:59:06 -0300

    add mock consumido no fetDetail.test

---
+++ __test__/mock/dataDetailMovie/dataDetailMovie.ts
@@ -0,0 +1,97 @@
+import { DetailData } from "../../../src/reducer/fetch/detail/interface";
+
+export const getSuccessDetailMoviePage1: DetailData = {
+  id: 609681,
+  title: "As Marvels",
+  original_title: "The Marvels",
+  release_date: "2023-11-08",
+  tagline: "Mais alto. Mais longe. Mais rápido. Juntas.",
+  genres: [
+    {
+      id: 878,
+      name: "Ficção científica",
+    },
+    {
+      id: 12,
+      name: "Aventura",
+    },
+    {
+      id: 28,
+      name: "Ação",
+    },
+  ],
+  popularity: "1399.352",
+  original_language: "en",
+  overview:
+    "Quando suas obrigações a levam a uma fenda espacial anômala ligada a um revolucionário.",
+  production_companies: [
+    {
+      id: 420,
+
+      name: "Marvel Studios",
+    },
+    {
+      id: 176762,
+
+      name: "Kevin Feige Productions",
+    },
+  ],
+  production_countries: [
+    {
+      iso_3166_1: "US",
+      name: "United States of America",
+    },
+  ],
+  poster_path: "/4mNFzHWQgMbonqSFhEtzbpnumFW.jpg",
+};
+
+export const getSuccessDetailMoviePage2: DetailData = {
+  id: 753342,
+  title: "Napoleão",
+  original_title: "Napoleon",
+  release_date: "2023-11-22",
+  tagline: "Ele veio do nada. Ele conquistou tudo.",
+  genres: [
+    {
+      id: 36,
+      name: "História",
+    },
+    {
+      id: 10752,
+      name: "Guerra",
+    },
+    {
+      id: 28,
+      name: "Ação",
+    },
+    {
+      id: 18,
+      name: "Drama",
+    },
+  ],
+  popularity: "641.688",
+  original_language: "en",
+  overview:
+    "Um olhar pessoal sobre as origens do líder militar francês e sua rápida e.",
+  production_companies: [
+    {
+      id: 1645,
+      name: "Scott Free Productions",
+    },
+    {
+      id: 194232,
+      name: "Apple Studios",
+    },
+  ],
+  production_countries: [
+    {
+      iso_3166_1: "GB",
+      name: "United Kingdom",
+    },
+    {
+      iso_3166_1: "US",
+      name: "United States of America",
+    },
+  ],
+  poster_path: "/oaUS0YJN3cHpvin1BMNmmnv0gRX.jpg",
+};
